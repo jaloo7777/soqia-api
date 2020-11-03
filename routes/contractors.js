75bce860827e776383b8b3e354c96c09409f0729
Abduljalil Barnawi <jaloo7777@gmail.com>
2020-11-03 22:00:54 +0530

    added Protect where only loged in user can create update delete && added getMe controllers

--- routes/contractors.js
+++ routes/contractors.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const {getContractors, getContractor,updateContractor,addContractor, deleteContractor ,createContractor} = require('../controllers/contractors');
-
+const {protect} = require('../middleware/auth')
 const router = express.Router({mergeParams: true});
 const {getWells,addeWell} = require('../controllers/wells')
 const wellRouter = require('./wells')
 const Contractor = require('../Model/Contractor')
 const advancedResults = require('../middleware/advancedResults')
+
+
 // router.use('/:contractorId/wells', wellRouter)
-router.route('/:contractorId/wells').get(getWells).post(addeWell)
+router.route('/:contractorId/wells').get(getWells).post(protect, addeWell)
 router.route('/')
 .get(advancedResults(Contractor,{path:'wells', select: 'name'}) ,getContractors)
-.post(createContractor)
-.post(addContractor)
+.post(protect, createContractor)
+.post(protect, addContractor)
 
 router.route('/:id')
 .get(getContractor)
-.delete(deleteContractor)
-.put(updateContractor)
+.delete(protect, deleteContractor)
+.put(protect, updateContractor)
 
 
 
--- routes/wells.js
+++ routes/wells.js
@@ -5,6 +5,7 @@ const router = express.Router({mergeParams: true});
 const {getWell,getWells,createWell,updateWell,deleteWell, addeWell} = require('../controllers/wells')
 const Well = require('../Model/Well')
 const advancedResults = require('../middleware/advancedResults')
+const {protect} = require('../middleware/auth')
 // Include other resource routers
 const contractorRouter = require('./contractors')
 // // Re-route into other resource routers &&& we have to mergeParam: true in the root of the file
@@ -13,13 +14,13 @@ router.use('/:wellId/contractors', contractorRouter)
 
 router.route('/')
 .get(advancedResults(Well,{path: 'contractor',select: ' executor '}), getWells)
-.post(createWell)
-.post(addeWell)
+.post(protect ,createWell)
+.post(protect,addeWell)
 
 router.route('/:id')
 .get(getWell)
-.delete(deleteWell)
-.put(updateWell)
+.delete(protect,deleteWell)
+.put(protect,updateWell)
 
 
 
