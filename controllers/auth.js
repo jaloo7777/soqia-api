3d89f4b1e20ddb412d6aac7f612511ecd9927fa7
Abduljalil Barnawi <jaloo7777@gmail.com>
2020-11-03 19:41:43 +0530

    implemented login in functionallity

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,7 @@ const asyncHandler = require('../middleware/asyncErrorHandler');
  
 
 // @desc     Register a User
-// @route    Post /api/v1/users
-// @route    Post /api/v1/users
+// @route    Post /api/v1/auth/register
 // @access   Public
 exports.register = asyncHandler(async (req,res,next) => {
     const {name,role,email,password} = req.body
@@ -19,4 +18,37 @@ exports.register = asyncHandler(async (req,res,next) => {
         data: user,
         token
     })
+})      
+
+
+// @desc     Register a User
+// @route    Post /api/v1/auth/login
+// @access   Public
+exports.login = asyncHandler(async (req,res,next) => {
+    const {email,password} = req.body;
+
+    if(!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password',400))
+    }
+
+    // Check for user
+    const user =  await User.findOne({email}).select('+password')
+    if(!user) {
+        return next(new ErrorResponse('Invalid credentials',401))
+    }
+
+    //Check password matches
+    const isMatch = await user.matchPassword(password)
+    if(!isMatch) {
+        return next(new ErrorResponse('Invalid credentials',401))
+
+    }
+    // Create token
+    const token = user.getSignedJwtToken();
+    
+    res.status(200).json({
+        success: true,
+        token
+    })
+ 
 })      
